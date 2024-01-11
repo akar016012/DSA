f74d719212b53478df2a56330741906993243ca8
Aditya Kar <61394999+akar016012@users.noreply.github.com>
2024-01-11 07:50:54 -0500

    added prepend, popfirst, and get

--- DblLinkedList.py
+++ DblLinkedList.py
@@ -39,6 +39,51 @@ class DBLLinkedList:
         self.length -= 1
         return temp
 
+    # Prepend
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.length += 1
+        return True
+
+    #  Pop First
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+            temp.next = None
+        self.length -= 1
+        return temp
+
+    # Get
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        if index < self.length / 2:
+            for _ in range(index):
+                temp = temp.next
+        else:
+            temp = self.tail
+            for _ in range(self.length - 1, index, -1):
+                temp = temp.prev
+        return temp
+
+    # Set value
+    # def set_value
+
     # print Doubly linked list
     def print(self):
         temp = self.head
--- playground.py
+++
@@ -1,6 +0,0 @@
-# Ask user for their name
-name = input("What is your name? \n").strip().title()
-
-name = name.split()
-
-print(f"hello, {name}")
