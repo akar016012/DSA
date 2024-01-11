be0f2edf2b4a25da9221f6129a96ba7d3e5327bb
Aditya Kar <61394999+akar016012@users.noreply.github.com>
2024-01-11 17:24:32 -0500

    modified dbl linkedlist

--- DblLinkedList.py
+++ DblLinkedList.py
@@ -82,7 +82,31 @@ class DBLLinkedList:
         return temp
 
     # Set value
-    # def set_value
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        before = self.get(index - 1)
+        after = before.next
+
+        new_node.prev = before
+        new_node.next = after
+        before.next = new_node
+        after.prev = new_node
+
+        self.length += 1
+        return True
 
     # print Doubly linked list
     def print(self):
